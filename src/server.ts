1776560d13dc0b1c6d72cdbf0e4d2ba27e7a65b7
Anders <ander.webmaster@uninove.edu.br>
2020-06-10 03:02:42 -0300

    Configurado padronização de codigo com: Eslint - Prettier - EditorConfig

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
-
-app.get('/', (req,res) =>{
-    return res.json({msg: 'hello world...'})
-})
+app.use(routes)
 
 app.listen(3333, () => {
     console.log('🚀 Server started on port 3333!')
