54f7583cfe094f537ff9cd9e864f1680300a0dbf
Anders <ander.webmaster@uninove.edu.br>
2020-06-10 03:29:49 -0300

    Analise e debug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,12 @@ import { Router } from "express"
 
 const routes = Router()
 
-routes.get('/', (req,res) => res.json({msg: 'hello world...'}))
+routes.post('/users', (req,res) => {
+  const { name,email } = req.body
+  const user = {
+    name,email
+  }
+  res.json(user)
+})
 
 export default routes
