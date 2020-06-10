34c0e73e1d3cfcfc9381cb66e982090b96c5779d
AnDeRs <ander.webmaster@uninove.edu.br>
2020-06-10 15:36:21 -0300

    Incluido rota de teste 'Hello Anders'

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,9 @@ import { Router } from "express"
 
 const routes = Router()
 
+routes.get('/', (req,res) =>{
+  res.json({msg:'Hello Anders...'})
+})
 routes.post('/users', (req,res) => {
   const { name,email } = req.body
   const user = {
